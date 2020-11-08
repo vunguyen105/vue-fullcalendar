fa6cf6906f21ecce3adc26ef835b34fef1d99e8c
nguyenvu <o0nguyen1150o@gmail.com>
2020-11-09 03:33:02 +0700

    change class

--- demo/app.vue
+++ demo/app.vue
@@ -1,54 +1,71 @@
 <template>
-  <div>
-  	<p>
-  		Here is the demo of vue-fullcalendar, no jquery fullcalendar.js required!
-  	</p>
-    <full-calendar class="test-fc" :events="fcEvents" lang="en"></full-calendar>
-  </div>
+    <div>
+        <p>
+            Here is the demo of vue-fullcalendar, no jquery fullcalendar.js required!
+        </p>
+        <full-calendar class="test-fc" :events="fcEvents"
+                       lang="en"></full-calendar>
+    </div>
 </template>
 <script>
-let demoEvents = [
+  let demoEvents = [
     {
-      title : 'Sunny 728-730',
-      start : '2016-07-25',
-      end : '2016-07-27'
+      title: 'Sunny 728-730',
+      start: '2020-07-25',
+      end: '2020-07-27'
     },
     {
-      title : 'Sunny 726-727',
-      start : '2016-07-26',
-      end : '2016-07-27'
+      title: 'Sunny 726-727',
+      start: '2020-11-11',
+      end: '2020-11-12',
+      classEvent: 'xxx'
     },
     {
-      title : 'Sunny 727-728',
-      start : '2016-07-27',
-      end : '2016-07-28'
+      title: 'Sunny 727-728',
+      start: '2020-11-11',
+      end: '2020-11-12'
     },
     {
-      title : 'Sunny 728-730',
-      start : '2016-07-28',
-      end : '2016-07-30'
+      title: 'Sunny 727-12213',
+      start: '2020-11-11',
+      end: '2020-11-12'
+    },
+    {
+      title: 'Sunny 727-22222',
+      start: '2020-11-11',
+      end: '2020-11-12'
+    },
+    {
+      title: 'Sunny 727-728',
+      start: '2020-11-11',
+      end: '2020-11-12'
+    },
+    {
+      title: 'Sunny 728-730',
+      start: '2020-11-11',
+      end: '2020-11-12'
     },
   ]
 
-export default {
-	data () {
-		return {
-      name:'Sunny!',
-      fcEvents : demoEvents
+  export default {
+    data() {
+      return {
+        name: 'Sunny!',
+        fcEvents: demoEvents
+      }
+    },
+    events: {
+      'changeMonth'(start, end, current) {
+        console.log('changeMonth', start, end, current)
+      }
+    },
+    components: {
+      'full-calendar': require('src/fullCalendar')
     }
-	},
-  events : {
-    'changeMonth' (start, end, current) {
-      console.log('changeMonth', start, end, current)
-    }
-  },
-  components : {
-    'full-calendar' : require('src/fullCalendar')
   }
-}
 </script>
 <style lang='scss'>
-  .app{
-    color:green;
-  }
+    .app {
+        color: green;
+    }
 </style>
